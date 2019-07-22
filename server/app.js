e57efac42c6c1058f20ccb2677767b52ef95137c
Manuel Blanco <manuel.22.lamas@gmail.com>
2019-07-22 18:30:28 -0500

    added dotenv to project and catching conection errors to mongoDB

--- server/app.js
+++ server/app.js
@@ -3,15 +3,17 @@ const express = require('express')
 const schema = require('./schema/schema.js')
 const graphqlHTTP = require('express-graphql')
 const mongoose = require('mongoose')
-
+require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 4000 || 4002 || 5002
 
 mongoose.connect(process.env.DB_USER)
 
-mongoose.connection.once('open', () => {
-  console.log(chalk.red('Connected to database!'))
-})
+mongoose.connection
+  .once('open', () => {
+    console.log(chalk.red('Connected to database!'))
+  })
+  .catch(err => console.error(err))
 
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }))
 
